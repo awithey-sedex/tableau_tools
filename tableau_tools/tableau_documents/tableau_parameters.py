a979aafcde6bd9ec3d01d94cc3f45f7400db3e44
Andrew Withey <andrew.withey@sedexglobal.com>
2019-09-16 09:21:54 +0100

    Merge from upstream

--- tableau_tools/tableau_documents/tableau_parameters.py
+++ tableau_tools/tableau_documents/tableau_parameters.py
@@ -8,10 +8,7 @@ from xml.sax.saxutils import quoteattr, unescape
 import datetime
 import collections
 import re
-<<<<<<< HEAD:tableau_tools/tableau_documents/tableau_parameters.py
 
-=======
->>>>>>> upstream/4.0.0:tableau_documents/tableau_parameters.py
 
 class TableauParameters(TableauDocument):
     def __init__(self, datasource_xml=None, logger_obj=None):
@@ -41,25 +38,15 @@ class TableauParameters(TableauDocument):
             self.log(u'Parameter XML passed in, finding essential characteristics')
             self.ds_xml = datasource_xml
             params_xml = self.ds_xml.findall(u'./column')
-<<<<<<< HEAD:tableau_tools/tableau_documents/tableau_parameters.py
-            numberedParameterRegex = re.compile('\[Parameter (\d+)\]')
-=======
             numbered_parameter_regex = re.compile(u"\[Parameter (\d+)\]")
->>>>>>> upstream/4.0.0:tableau_documents/tableau_parameters.py
             for column in params_xml:
                 alias = column.get(u'caption')
                 internal_name = column.get(u'name')  # type: unicode
                 # Parameters are all given internal name [Parameter #], unless they are copies where they
                 # end with (copy) h/t Jeff James for discovering
-<<<<<<< HEAD:tableau_tools/tableau_documents/tableau_parameters.py
-                regexMatch = numberedParameterRegex.match(internal_name)
-                if regexMatch and regexMatch.group(1):
-                    param_num = int(regexMatch.group(1))
-=======
                 regex_match = numbered_parameter_regex.match(internal_name)
                 if regex_match and regex_match.group(1):
                     param_num = int(regex_match.group(1))
->>>>>>> upstream/4.0.0:tableau_documents/tableau_parameters.py
                     # Move up the highest_param_num counter for when you add new ones
                     if param_num > self._highest_param_num:
                         self._highest_param_num = param_num
